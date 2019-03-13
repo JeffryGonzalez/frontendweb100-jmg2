57060514a80a5c351043ecb26ac70c720dd4a3df
Jeff Gonzalez <jeff@Hypertheory>
2019-03-13 15:55:48 -0400

    end of second day

---
+++ spec/utils.ts
@@ -0,0 +1,7 @@
+type Transformer = (x: string) => string;
+export function formatName(
+    first: string,
+    last: string,
+    transformer: Transformer = (x) => x): string {
+    return transformer(`${last}, ${first}`);
+}
